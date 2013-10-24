95f64520c1b2ed13bb1e96e50605109c0ca25cbf
mlaursen <mlaursen03@gmail.com>
2013-10-24 12:20:57 -0400

    added logging pages 'hopefully'

--- betteru/urls.py
+++ betteru/urls.py
@@ -1,18 +1,18 @@
 from django.conf.urls import patterns, include, url
 from django.views.generic import TemplateView
 
+
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
 # admin.autodiscover()
 
-from accounts import views
 
 urlpatterns = patterns('',
     url(r'^$', TemplateView.as_view(template_name="index.html")),
     url(r'^info/$', TemplateView.as_view(template_name="info.html")),
     url(r'^stats/', include('stats.urls', namespace="stats")),
-    url(r'^login/$', views.login, name='login'),
-    url(r'^logout/$', views.logout, name='logout'),
+    url(r'^login/$', 'django.contrib.auth.views.login', name='login'),
+    url(r'^logout/$', 'django.contrib.auth.views.logout', name='logout'),
     #url(r'^login/', include('accounts.urls', namespace="accounts")),
     #url(r'^logout/', include('accounts.urls', namespace="accounts")),
     url(r'^settings/', include('accounts.urls', namespace="accounts")),
