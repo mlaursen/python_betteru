8f7d1098962fe25bbbecd18dc1970427529bf710
mlaursen <mlaursen03@gmail.com>
2013-10-27 12:28:45 -0400

    giving up on custom tags for now

--- utils/templatetags/util_tags.py
+++ utils/templatetags/util_tags.py
@@ -1,7 +1,14 @@
 from django import template
 
+from utils.util import HtmlTag
+
 register = template.Library()
 
 @register.simple_tag
-def example(a1):
-    return "string a1: %s" % a1
+def tag(name, content='', tclass=False, tid=False, other=False):
+    return HtmlTag(name, content, tclass, tid, other).print()
+
+@register.simple_tag
+def submit(init='Submit'):
+    return tag( 'button', init, 'btn', False, {'type': 'submit'}, True)
+
