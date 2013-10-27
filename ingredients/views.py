a7c079d584754127ee340b62c06d457fec268ea7
mlaursen <mlaursen03@gmail.com>
2013-10-27 14:38:33 -0400

    ingredient tables update

--- ingredients/views.py
+++ ingredients/views.py
@@ -4,10 +4,22 @@ from django.core.urlresolvers import reverse
 from django.views import generic
 from django.utils import timezone
 
-from stats.models import Stats
+from ingredients.models import Ingredient, Category, Brand
 
 class IndexView(generic.base.TemplateView):
     template_name = 'ingredients/index.html'
 
 class AddView(generic.base.TemplateView):
     template_name = 'ingredients/add.html'
+
+def index(request):
+    ingredients = Ingredient.objects.all()
+    brands = Brand.objects.all()
+    categories = Category.objects.all()
+    return render(request,
+            'ingredeints/index.html',
+            {'ingredients': ingredients,
+                'brands': brands,
+                'categories': categories}
+    )
+
