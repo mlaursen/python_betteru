aadb51e033f460bf54a30b88373860b0617d3bd6
mlaursen <mlaursen03@gmail.com>
2013-10-28 20:19:58 -0400

    ajax working bruh

--- ingredients/views.py
+++ ingredients/views.py
@@ -8,7 +8,7 @@ from django.utils import timezone
 from ingredients.forms import CreateForm
 from ingredients.models import Ingredient, Category, Brand
 
-import sys
+import sys, re
 
 def index(request):
     if request.is_ajax():
@@ -37,24 +37,28 @@ def create(request):
     return render(request, 'ingredients/create.html', {'form': f},)
 
 def load_table(request):
+    brands = Brand.objects.all().order_by('name')
+    categories = Category.objects.all()
     if request.is_ajax():
         category = request.GET.get('category')
         brand    = request.GET.get('brand')
-        if category != 'allcategories' and category != '' and brand != 'allbrands' and brand != '':
-            ingredients = Ingredient.objects.filter(pk=1)
-            #ingredients = Ingredient.objects.filter(category=category, brand=brand)
-        elif category != 'allcategories' and category != '':
+        if category and not match_all(category) and brand and not match_all(brand):
+            ingredients = Ingredient.objects.filter(category=category, brand=brand)
+        elif category and not match_all(category):
             ingredients = Ingredient.objects.filter(category=category)
-        elif brand != 'allbrands' and brand != '':
+        elif brand and not match_all(brand):
             ingredients = Ingredient.objects.filter(brand=brand)
         else:
             ingredients = Ingredient.objects.all()
-        return 'this'
-        #return render_to_response('ingredients/index.html',
-        #    {'ingredients': ingredients,
-        #        'brands': brands,
-        #        'categories': categories},
-        #    context_instance=RequestContext(request))
+        return render_to_response('ingredients/ingredient_table.html',
+            {'ingredients': ingredients,
+                'brands': brands,
+                'categories': categories},
+            context_instance=RequestContext(request))
     else:
         message = "This is not ajax"
-        return message
+        return HttpResponse('Goodbye')
+
+def match_all(n):
+    return re.match("[A-z]*all", n)
+
