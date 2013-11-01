da2a475f6904165f59182b1571c864a678039ed9
mlaursen <mlaursen03@gmail.com>
2013-11-01 10:32:58 -0400

    updated for- to for= and working on edit account template

--- accounts/forms.py
+++ accounts/forms.py
@@ -65,12 +65,10 @@ class LoginForm(Form):
         return valid
 
 class EditAccountForm(ModelForm):
-
     class Meta:
         model = Account
-        fields = ['birthday', 'gender', 'units', 'height', 'activity_multiplier']
+        fields = ['birthday', 'height', 'activity_multiplier']
         widgets = { 
             'birthday': forms.DateInput(format=('%dd/%mm/%Y'),
                                         attrs={'placeholder': 'Select a date'}),
-            'gender': forms.ChoiceField(choices=('Male', 'Female'))
     }
