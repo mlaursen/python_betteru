66e7953b37649080c0019d38c5b135ae8b6f1530
mlaursen <mlaursen03@gmail.com>
2013-11-17 12:01:14 -0500

    uncommented the end of create_mealpart.. woops

--- meals/tests.py
+++ meals/tests.py
@@ -8,7 +8,7 @@ Replace this with more appropriate tests for your application.
 from django.test import TestCase
 from meals.models import *
 from ingredients.models import *
-from ingredients.tests import IngredientsTests, create_test_ingredient
+from ingredients.tests import create_test_ingredient
 
 
 class MealsTest(TestCase):
