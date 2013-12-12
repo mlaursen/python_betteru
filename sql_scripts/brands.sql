8a45f7eff88fc300958474afc475cd3e5838f97e
mlaursen <mlaursen03@gmail.com>
2013-12-11 22:20:18 -0500

    updated initial data

--- sql_scripts/brands.sql
+++ sql_scripts/brands.sql
@@ -3,11 +3,14 @@
 --------------------------------------------------------
 REM INSERTING into BETTERU.INGREDIENTS_BRAND
 SET DEFINE OFF;
-Insert into BETTERU.INGREDIENTS_BRAND (ID,NAME) values (21,'Chobani');
 Insert into BETTERU.INGREDIENTS_BRAND (ID,NAME) values (1,'Isopure');
 Insert into BETTERU.INGREDIENTS_BRAND (ID,NAME) values (2,'Regal Spring');
 Insert into BETTERU.INGREDIENTS_BRAND (ID,NAME) values (3,'Food City');
 Insert into BETTERU.INGREDIENTS_BRAND (ID,NAME) values (4,'Mahatma');
 Insert into BETTERU.INGREDIENTS_BRAND (ID,NAME) values (5,'Healthy Harvest');
-Insert into BETTERU.INGREDIENTS_BRAND (ID,NAME) values (6,'Nature''s Own');
-Insert into BETTERU.INGREDIENTS_BRAND (ID,NAME) values (10,'Quaker Oats');
+INSERT INTO BETTERU.INGREDIENTS_BRAND (ID,NAME) VALUES (6,'Nature''s Own');
+Insert into BETTERU.INGREDIENTS_BRAND (ID,NAME) values (7,'Chobani');
+Insert into BETTERU.INGREDIENTS_BRAND (ID,NAME) values (8,'Quaker Oats');
+
+
+exec RESET_SEQ('INGREDIENTS_BRAND');
--- sql_scripts/meals.sql
+++ sql_scripts/meals.sql
@@ -2,11 +2,11 @@
 --  File created - Tuesday-November-19-2013   
 --------------------------------------------------------
 REM INSERTING into BETTERU.MEALS_MEAL
-SET DEFINE OFF;
-Insert into BETTERU.MEALS_MEAL (ID,NAME) values (4,'Protein shake w/ greek yogurt, milk, and oats');
-Insert into BETTERU.MEALS_MEAL (ID,NAME) values (6,'Isopure Protein Shake with Oats, Milk, and Greek Yogurt');
-Insert into BETTERU.MEALS_MEAL (ID,NAME) values (5,'200g Chicken Breast');
-Insert into BETTERU.MEALS_MEAL (ID,NAME) values (2,'Protein Shake with Milk');
-Insert into BETTERU.MEALS_MEAL (ID,NAME) values (1,'300g Chicken Breast');
-Insert into BETTERU.MEALS_MEAL (ID,NAME) values (21,'Isopure Protein Shake with Milk and Greek Yogurt');
-Insert into BETTERU.MEALS_MEAL (ID,NAME) values (3,'2 eggs');
+set DEFINE off;
+INSERT INTO BETTERU.MEALS_MEAL (id,name,DESCRIPTION) values (1,'300g Chicken Breast','300g chicken breast with random seasonings and veggies.');
+INSERT INTO BETTERU.MEALS_MEAL (id,name,DESCRIPTION) values (2,'Protein shake w/ greek yogurt, milk, and oats','Isopure protein with 12 oz milk and 227g fat free greek yogurt in a blender');
+INSERT INTO BETTERU.MEALS_MEAL (id,name,DESCRIPTION) values (3,'200g Chicken Breast','200g chicken breast with random seasonings and veggies.');
+INSERT INTO BETTERU.MEALS_MEAL (id,name,DESCRIPTION) values (4,'Isopure Protein Shake with Milk and Greek Yogurt','Isopure protein with 12 oz milk, 40g oats and 227g fat free greek yogurt in a blender');
+INSERT INTO BETTERU.MEALS_MEAL (ID,NAME,DESCRIPTION) values (5,'2 eggs','2 hard boiled eggs.');
+
+exec RESET_SEQ('MEALS_MEAL');
