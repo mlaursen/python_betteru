0ab9a7c91875f14d5f3360f19c702c87cb9c507f
mlaursen <mlaursen03@gmail.com>
2013-12-11 22:58:46 -0500

    initial data and script working

--- startup_script.bat
+++ startup_script.bat
@@ -6,7 +6,6 @@ python manage.py syncdb
 type sql_scripts\procedures.sql | python manage.py dbshell
 type sql_scripts\brands.sql | python manage.py dbshell
 type sql_scripts\categories.sql | python manage.py dbshell
-type sql_scripts\views.sql | python manage.py dbshell
 
 mv ingredients\models.py ingredients\.syncdb.py
 mv ingredients\models_tmp.py ingredients\models.py
@@ -16,5 +15,6 @@ type sql_scripts\accounts.sql | python manage.py dbshell
 type sql_scripts\ingredients.sql | python manage.py dbshell
 type sql_scripts\meals.sql | python manage.py dbshell
 type sql_scripts\mealparts.sql | python manage.py dbshell
+type sql_scripts\views.sql | python manage.py dbshell
 
 python manage.py syncdb
